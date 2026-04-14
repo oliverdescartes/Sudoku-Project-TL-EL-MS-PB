ae186d1d38d91282fb47d56bc5fdafa31aa6da3f
Tommy Lin <tommy@Tommys-MacBook-Air.local>
2026-04-14 15:41:42 -0400

    panther 1: first function done

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,7 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.removed_cells = removed_cells
 
     '''
 	Returns a 2D python list of numbers which represents the board
